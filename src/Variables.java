467b7f811eab930a7623cd635cc2d0ce2d8d37ba
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2021-05-27 17:34:26 +0530

    variables

---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+
+    public static void main(String[]args){
+
+        int a = 7; // 4bytes
+        short b = 2; // 2 bytes
+
+        System.out.println(a);
+        a = 48;
+        System.out.println(a);
+
+        double dec = 7.1;
+
+        float fl = 7.1f;
+
+        boolean bool = true;
+
+        char c = 'A';
+
+    }
+}
