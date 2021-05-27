e9302b71ce6a777a29494fdc4a717cdcbd9c29e6
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2021-05-27 23:46:09 +0530

    loops

---
+++ src/Loops.java
@@ -0,0 +1,19 @@
+public class Loops {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i<=7; i++){
+            System.out.println(i);
+        }
+
+        int i = 7;
+
+        while(i>0){
+            System.out.println(i);
+            i++;
+        }
+
+
+    }
+
+}
