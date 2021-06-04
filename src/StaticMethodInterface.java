320329e74659d73144d3869e9674c34d77634f41
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2021-06-04 17:14:08 +0530

    static method with interface

---
+++ src/StaticMethodInterface.java
@@ -0,0 +1,16 @@
+
+
+interface StaticMethod{
+    static void print(){
+        System.out.println("In Static method");
+    }
+}
+
+
+public class StaticMethodInterface {
+
+    public static void main(String[] args) {
+        StaticMethod.print();
+    }
+
+}
