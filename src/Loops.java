be743617b70fd5da9cb1ffa6892037017fbc69bd
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2021-06-04 17:40:15 +0530

    package:

--- src/Loops.java
+++ src/Loops.java
@@ -1,17 +1,22 @@
+import com.PackageDemo.PackageLoops;
+
 public class Loops {
 
     public static void main(String[] args) {
 
-        for(int i=0; i<=7; i++){
+        for(int i=0; i<=4; i++){
             System.out.println(i);
         }
 
         int i = 7;
 
-        while(i>0){
-            System.out.println(i);
-            i++;
-        }
+//        while(i>0){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        PackageLoops obj = new PackageLoops();
+        obj.loops(10);
 
 
     }
---
+++ src/com/PackageDemo/PackageLoops.java
@@ -0,0 +1,12 @@
+package com.PackageDemo;
+
+public class PackageLoops{
+    public void loops(int n){
+
+        for(int i=0; i<n; i++){
+            System.out.println(i);
+        }
+
+    }
+}
+
